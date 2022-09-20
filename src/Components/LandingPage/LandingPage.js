a667f69746009bb3119e14d453e19f9ce43881be
Logan Banning <loganboganb@gmail.com>
2022-09-19 20:47:23 -0600

    removed backend files

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -28,7 +28,8 @@ const LandingPageComponent = (props) => {
   ];
 
   const handleLogin = () => {
-    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=${scopes.join("%20")}&show_dialog=true`;
+    // window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=${scopes.join("%20")}&show_dialog=true`;
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&show_dialog=true`;
   };
 
   return (
--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,34 +1,10 @@
 import React from "react";
-import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
-import { useToken, SpotifyURL, useProfile } from "../../utils";
+import { Link } from "react-router-dom";
 import SideBarPlaylist from "./SideBarPlaylist";
 import Logo from "../../images/Spotify_Logo_RGB_White.png";
 import "./SideBar.css";
 
 const SideBarComponent = () => {
-  const token = useToken();
-  const user = useProfile();
-  const history = useHistory();
-
-  const createPlaylist = () => {
-    try {
-      axios.post(
-        `${SpotifyURL}/users/${user.id}/playlists`,
-        {
-          name: "New Playlist",
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      history.go(0);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div className="side-bar">
@@ -52,12 +28,6 @@ const SideBarComponent = () => {
         </Link>
       </div>
       <div className="playlist-liked-songs">
-        <button
-          className="create-playlist-btn side-bar-btn"
-          onClick={createPlaylist}
-        >
-          Create Playlist
-        </button>
         <Link to="/liked-songs">
           <button className="liked-songs-btn side-bar-btn">Liked Songs</button>
         </Link>
