33714e60843f604e483194d1aedd5151ed9bb24a
Logan Banning <loganboganb@gmail.com>
2022-09-20 20:42:42 -0600

    restored utils page

--- src/utils.js
+++ src/utils.js
@@ -3,56 +3,15 @@ import axios from "axios";
 
 export const SpotifyURL = "https://api.spotify.com/v1";
 
-const {
-  REACT_APP_CLIENT_ID,
-  REACT_APP_CLIENT_SECRET,
-  REACT_APP_REDIRECT_URL,
-} = process.env;
-
-const exchangeCodeForSpotifyToken = async (code) => {
-  try {
-    const response = await axios.post("https://accounts.spotify.com/api/token", {
-    code: code,
-    redirect_uri: REACT_APP_REDIRECT_URL,
-    grant_type: "authorization_code",
-  },{ 
-    headers: {
-    Authorization:
-      "Basic " +
-      Buffer.from(REACT_APP_CLIENT_ID + ":" + REACT_APP_CLIENT_SECRET).toString(
-        "base64"
-      ),
-    "Content-Type": "application/x-www-form-urlencoded",
-  }
-  });
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 export const useToken = () => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     async function getToken() {
       try {
-        // Instead of calling backend for the token
-
-        // Check local storage for the token, if we have it return it
-        let spotifyToken = localStorage.getItem('spotify_token');
-
-        if(!spotifyToken) {
-          // If it's not in local storage check the query parameter for a 'code' 
-          const code = new URLSearchParams(this.props.location.search).get("code")
-          console.log('got the code', code)
-          // if that is there there exchange w/Spotify for a token and save it
-          spotifyToken = exchangeCodeForSpotifyToken(code);
-        } else {
-          // What if the token is expired or close to expiring???? Figure that out later
-          setToken(spotifyToken)
-        }
-      } catch (error) {
-
+        const response = await axios.get("/auth/token");
+        setToken(response.data.access_token);
+      } catch {
         return;
       }
     }
